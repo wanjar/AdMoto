f2485a4575aa6821159c1d6f12755646aa824069
wanjar <wanjangunjiri@gmail.com>
2023-09-21 13:50:31 +0300

    Initialize map on script.js

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -175,3 +175,19 @@ $('#layoutDirection .nav-link').on('click', function(e){
 
   window.location.href = newLoc;
 })
+
+
+//Load Google Maps
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
